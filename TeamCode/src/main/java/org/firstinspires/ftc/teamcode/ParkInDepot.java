4cce09abaf9010d278f552f26484bd24a9bba12f
Mecaneer23 <Mecaneer23@gmail.com>
2021-12-18 18:51:53 -0600

    disable parkindepot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkInDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkInDepot.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.AutoBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @Autonomous
 public class ParkInDepot extends LinearOpMode {
     
