f4cd5711dab173acc15dd4c13d66e417c0c0c584
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-13 15:33:56 -0600

    add telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drive.java
@@ -57,6 +57,9 @@ public class Mecanum_Drive extends LinearOpMode {
             backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          
+            telemetry.addData("Control Scheme", "\nleft stick - xy position of robot\nright stick - rotation of robot\nright bumper - 1/2 speed slowmode\nleft bumper - 1/4 speed slowmode\ndpad - 1.0 power in any given direction\nclick stick - rotate that direction\n\tnone - 90\n\ty - 45\n\tb - 120\n\ta - 180\nback - turn left\nguide - turn right");
+          
             while (opModeIsActive()) {
                 x = gamepad1.left_stick_x;
                 y = -gamepad1.left_stick_y;
